ced4cb4c98803e6e211a36926cc98b99c88f5c22
Adrien Baur <abaur@student.42.fr>
2019-12-02 14:17:33 +0100

    w_string now handles precision

--- w_string.c
+++ w_string.c
@@ -6,7 +6,7 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/22 17:17:57 by abaur             #+#    #+#             */
-/*   Updated: 2019/12/02 13:41:28 by abaur            ###   ########.fr       */
+/*   Updated: 2019/12/02 14:13:41 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,15 @@ int	w_string(t_pftag *tag)
 {
 	char	*arg;
 	int		count;
+	int		i;
 
 	count = 0;
+	i = 0;
 	arg = (char*)tag->argument;
 	initbuffer(tag);
 	if (!arg)
 		arg = "(null)";
-	while (*arg)
+	while (*arg && (tag->precision < 0 || i++ < tag->precision))
 	{
 		count += tag->printer(tag, *arg);
 		arg++;
